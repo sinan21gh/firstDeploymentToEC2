b919f6b39cc7fa92d715667b84db300d4502e1c3
Sinan <sinanikleriya18@gmail.com>
2025-09-06 19:41:41 +0100

    1st commit

---
+++ src/test/java/com/sinans/awsdeploydemo/AwsDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinans.awsdeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
